7334c845fae0eff8327966074ee731e194e6117e
odinstudent <34312554+Alpattex@users.noreply.github>
2023-10-05 00:57:23 +0300

    Navbar Styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,48 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 
 const Container = styled.div`
     height: 60px;
-    background-color: black;
+`;
+
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Left = styled.div`
+    flex: 1;
+`;
+
+const Language = styled.span`
+    font-size: 14px;
+    cursor: pointer;
+`;
+
+
+
+const Center = styled.div`
+    flex: 1;
+`;
+const Right = styled.div`
+    flex: 1;
 `;
 
 const Navbar = () => {
   return (
     <Container>
-        navbar
+        <Wrapper>
+            <Left>
+                <Language>English</Language>
+                <SearchContainer>
+                    input
+                    
+                </SearchContainer>
+            </Left>
+            <Center>Center</Center>
+            <Right>Right</Right>
+        </Wrapper>
     </Container>
   );
 };
